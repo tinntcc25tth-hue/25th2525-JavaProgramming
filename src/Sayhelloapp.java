7ecff8e1b5d6a2eb40615e4c92208922de72327d
tinntcc25tth-hue <tin.nt.cc25tth@ntu.edu.vn>
2026-08-08 15:45:22 +0700

    Xin chao java

---
+++ src/Sayhelloapp.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class Sayhelloapp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chao. Toi la cac SV Khoa 25 CNTT-NTU: Hoc Lap trinh Java");
+        System.out.print("Nguyễn Trung Tín");
+	}
+
+}
---
+++ src/till/edu/Xinchao.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package till.edu;
+
+/**
+ * 
+ */
+public class Xinchao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// 
+		System.out.println("TÔI LẦN ĐẦU HỌC Java, và đang thử nghiệm quản lý mã nguồn với GIT");
+        System.out.print("Tôi thêm một dòng mới, và sửa lại chữ java ở trên");
+	}
+
+}
