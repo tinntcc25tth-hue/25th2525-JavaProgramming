40e5ffa3ce589e55845042946e62033f1bdbdab9
tinntcc25tth-hue <tin.nt.cc25tth@ntu.edu.vn>
2026-08-10 11:55:29 +0700

    Thêm mã nhập, khai báo biến

--- Baitap1-NhapXuat_TinhTong2So/src/TonghaiSo.java
+++ Baitap1-NhapXuat_TinhTong2So/src/TonghaiSo.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
 
 public class TonghaiSo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Khai báo biến
+		double num1, num2, sum;
+		
+		// khai báo đối tượng Scanner
+		Scanner banphim = new Scanner(System.in);
+		
+		// In ra thông tin, mời User nhập số thứ nhất
+		
+		System.out.print("Mời nhập số thứ nhất: ");
+		num1= banphim.nextDouble()
+				
+		System.out.print("Mời nhập số thứ hai: ");
+		num2= banphim.nextDouble()
+				
 
 	}
 
